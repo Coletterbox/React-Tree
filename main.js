9ec9258d0b085f176ccdede4242261d18ab86dcd
Colette <colette.rs.shaw@gmail.com>
2019-10-25 17:30:49 +0100

    Add JSX and JS versions of code snippet

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 import { PythagorasTree } from "./PythagorasTree.js";
 
+// JSX
 ReactDOM.render(
-  <PythagorasTree sprout={0.2} sway={0.44} />,
+  <a href="https://xkcd.com/221/">
+    <img src="https://imgs.xkcd.com/comics/random_number.png" />
+  </a>,
+  document.getElementById("app")
+);
+
+// JS
+ReactDOM.render(
+  React.createElement(
+    "a",
+    { href: "https://xkcd.com/221/" },
+    React.createElement("img", {
+      src: "https://imgs.xkcd.com/comics/random_number.png"
+    })
+  ),
   document.getElementById("app")
 );
